e52fd5132e6861141325e0e08f232fda5fe7924c
zhuanghaixin <649308293@qq.com>
2020-10-20 20:45:38 +0800

    feat(数据报表): 5-4引入element-ui和Echarts✅

--- datav-report/src/App.vue
+++ datav-report/src/App.vue
@@ -1,16 +1,23 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-  html,body,#app{
-    width:100%;
-    height: 100%;
-    margin: 0;
-    padding: 0;
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+  *{
     box-sizing: border-box;
   }
-
+html,body,#app{
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
 </style>
--- datav-report/src/main.js
+++ datav-report/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import './plugins/element.js'
+import ECharts from 'echarts'
 Vue.config.productionTip = false
+Vue.prototype.$echarts = ECharts // 这样每个组件在引入echarts 不需要每次都引入import Echarts from 'echarts'
 
 new Vue({
   router,
